44304f6fea4bc74193951b780177920740aa562b
root <root@nbouchin.localdomain>
2018-12-11 16:53:56 +0100

    Update

--- assigment09/main.c
+++ assigment09/main.c
@@ -17,13 +17,13 @@
 
 MODULE_LICENSE("GPL");
 
-#define PROC_NAME	"mymounts"
+#define PROC_NAME "mymounts"
 
 static void *my_seq_start(struct seq_file *s, loff_t *pos)
 {
 	static unsigned long counter = 0;
 
-	if ( *pos == 0 ) {	
+	if (*pos == 0) {
 		return &counter;
 	} else {
 		*pos = 0;
@@ -45,34 +45,31 @@ static void my_seq_stop(struct seq_file *s, void *v)
 
 static int my_seq_show(struct seq_file *s, void *v)
 {
-	struct mnt_namespace	*ns = current->nsproxy->mnt_ns;
-	struct mount		*mnt;
+	struct mnt_namespace *ns = current->nsproxy->mnt_ns;
+	struct mount *mnt;
 
-	list_for_each_entry(mnt, &ns->list, mnt_list) {
-		seq_printf(s, "%s\t\t%s\n", mnt->mnt_mountpoint->d_name.name, mnt->mnt_devname);
+	list_for_each_entry (mnt, &ns->list, mnt_list) {
+		seq_printf(s, "%s\t\t%s\n", mnt->mnt_mountpoint->d_name.name,
+			   mnt->mnt_devname);
 	}
 	return 0;
 }
 
-static struct seq_operations my_seq_ops = {
-	.start = my_seq_start,
-	.next  = my_seq_next,
-	.stop  = my_seq_stop,
-	.show  = my_seq_show
-};
+static struct seq_operations my_seq_ops = { .start = my_seq_start,
+					    .next = my_seq_next,
+					    .stop = my_seq_stop,
+					    .show = my_seq_show };
 
 static int my_open(struct inode *inode, struct file *file)
 {
 	return seq_open(file, &my_seq_ops);
 };
 
-static struct file_operations my_file_ops = {
-	.owner   = THIS_MODULE,
-	.open    = my_open,
-	.read    = seq_read,
-	.llseek  = seq_lseek,
-	.release = seq_release
-};
+static srruct file_operations my_file_ops = { .owner = THIS_MODULE,
+					      .open = my_open,
+					      .read = seq_read,
+					      .llseek = seq_lseek,
+					      .release = seq_release };
 
 int init_module(void)
 {
