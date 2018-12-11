87f7649e7d898b208ba430e0075f418972bad6c5
root <root@nbouchin.localdomain>
2018-12-11 10:24:46 +0100

    up

--- assigment09/main.c
+++ assigment09/main.c
@@ -7,24 +7,26 @@
 #include <linux/mount.h>
 #include <linux/fs_struct.h>
 #include <linux/proc_fs.h>
+#include <linux/slab.h>
 #include </usr/src/linux-next-next-20181210/fs/proc/internal.h>
 
 MODULE_LICENSE("GPL");
 
 struct dentry *mymounts = NULL;
 struct proc_dir_entry *proc_dir_entry = NULL;
+static char *ptr;
+static size_t ldata;
 
-ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp)
+ssize_t read_proc(struct file *filp, char __user *buff, size_t count, loff_t *offp)
 {
-	list_for_each_entry (mymounts, &current->fs->root.mnt->mnt_root->d_subdirs, d_child) {
-		if (mymounts->d_flags & DCACHE_MOUNTED) {
-			if (copy_to_user(buf, mymounts->d_name.name, count) != 0) {
-				return -EFAULT;
-			}
-			*offp += count;
-			printk("%s is mounted", mymounts->d_name.name);
-		}
+	if (*offp >= PAGE_SIZE * sizeof(char))
+		return 0;
+	if (*offp + count > ldata)
+		count = ldata - *offp;
+	if (copy_to_user(buff, ptr + *offp, count) != 0) {
+		return -EFAULT;
 	}
+	*offp += count;
 	return count;
 }
 
@@ -32,7 +34,25 @@ struct file_operations fops = { .read = read_proc };
 
 static int __init mountlist_init(void)
 {
+	size_t count;
+
+	count = 0;
+	if (!ptr) {
+		if (!(ptr = kmalloc(PAGE_SIZE * sizeof(char), GFP_KERNEL))) {
+			return -EFAULT;
+		}
+	}
+	memset(ptr, 0, PAGE_SIZE * sizeof(char));
 	proc_dir_entry = proc_create("mymounts", 0777, NULL, &fops);
+	list_for_each_entry (mymounts, &current->fs->root.mnt->mnt_root->d_subdirs, d_child) {
+		if (mymounts->d_flags & DCACHE_MOUNTED) {
+			ptr = strncat(ptr, mymounts->d_name.name, strlen(mymounts->d_name.name));	
+			count += strlen(mymounts->d_name.name);
+			*(ptr + count) = '\n';
+			count++;
+			ldata += count;
+		}
+	}
 	printk(KERN_INFO "mountlist module loaded.\n");
 	return 0;
 }
