8d0549239d0c3d36333aece48b505ce2eff066e8
root <root@nbouchin.localdomain>
2019-01-04 14:45:07 +0100

    up

--- assigment08/main.c
+++ assigment08/main.c
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -6,9 +5,9 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("LICENSE");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
-MODULE_DESCRIPTION("Useless module");
+MODULE_DESCRIPTION("reverse string module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 			 loff_t *offs);
@@ -16,36 +15,52 @@ static ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 			  loff_t *offs);
 
-static const struct file_operations myfd_fops = { .owner = THIS_MODULE,
-						  .read = &myfd_read,
-						  .write = &myfd_write };
+static struct file_operations myfd_fops = { .owner = THIS_MODULE,
+					    .read = &myfd_read,
+					    .write = &myfd_write };
 
 static struct miscdevice myfd_device = { .minor = MISC_DYNAMIC_MINOR,
 					 .name = "reverse",
 					 .fops = &myfd_fops };
 char str[PAGE_SIZE];
+char *tmp;
 
 static int __init myfd_init(void)
 {
-	return misc_register(&myfd_device);
+	int retval;
+
+	retval = misc_register(&(myfd_device));
+	pr_info("reverse device registered");
+	return 1;
 }
 
 static void __exit myfd_cleanup(void)
 {
-	misc_deregister(&myfd_device);
+	misc_deregister(&(myfd_device));
+	pr_info("reverse device deregistered");
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
-	return simple_read_from_buffer(user, size, offs, str,
-				       strnlen(str, PAGE_SIZE));
-}
+	size_t t, i;
+	char *tmp2;
 
+	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
+	tmp = tmp2;
+	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
+		tmp[i] = str[t];
+	}
+	tmp[i] = 0x0;
+	return simple_read_from_buffer(user, size, offs, tmp, i);
+}
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 		   loff_t *offs)
 {
-	return simple_write_to_buffer(str, size, offs, user, size);
-}
+	ssize_t res;
 
+	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
+	str[size + 1] = 0x0;
+	return res;
+}
 module_init(myfd_init);
 module_exit(myfd_cleanup);
